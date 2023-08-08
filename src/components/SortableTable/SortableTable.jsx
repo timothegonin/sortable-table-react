faec43101eebaa67370bdd70276b8aad47bf4b67
TimotheGonin <gonin.timothe@gmail.com>
2023-08-08 16:49:59 +0200

    💄 add basic SortableTable component

---
+++ src/components/SortableTable/SortableTable.jsx
@@ -0,0 +1,92 @@
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
+import Stack from "react-bootstrap/Stack";
+import Badge from "react-bootstrap/Badge";
+import Pagination from "react-bootstrap/Pagination";
+
+export const SortableTable = () => {
+	return (
+		<Container fluid="md">
+			{/* CONTROLS AND TABLE */}
+			<Form className="w-100 mb-4 ">
+				<Row className="d-flex flex-column-reverse flex-md-row justify-content-between">
+					<Col className="col-8 col-md-4 d-flex align-items-center">
+						Show
+						<Form.Select className="mx-3">
+							<option>10</option>
+							<option>25</option>
+							<option>50</option>
+							<option>100</option>
+						</Form.Select>
+						entries
+					</Col>
+					<Col className="col-8 mb-3 mb-md-0 col-md-3">
+						<Form.Control
+							type="search"
+							placeholder="Search"
+							aria-label="Search"
+						/>
+					</Col>
+				</Row>
+			</Form>
+			{/* <EmployeesTableView /> */}
+			<Table striped bordered hover responsive>
+				<thead>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Start Date</th>
+						<th>Department</th>
+						<th>Date of Birth</th>
+						<th>Street</th>
+						<th>City</th>
+						<th>State</th>
+						<th>Zip Code</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>employee firstName</td>
+						<td>employee lastName</td>
+						<td> employee startDate</td>
+						<td>employee department</td>
+						<td>employee dateOfBirth</td>
+						<td>employee street</td>
+						<td>employee city</td>
+						<td>employee state</td>
+						<td>employee zipCode</td>
+					</tr>
+				</tbody>
+			</Table>
+
+			{/* INFOS + PAGINATION */}
+			<Stack
+				direction="horizontal"
+				gap={3}
+				className="my-3 d-flex flex-column flex-md-row justify-content-md-between"
+			>
+				<Badge bg="primary">Showing 1 to 1 of 1 entries</Badge>
+				<Pagination size="sm" className="md-ms-auto my-auto">
+					<Pagination.First />
+					<Pagination.Prev />
+					<Pagination.Item>{1}</Pagination.Item>
+					<Pagination.Ellipsis />
+
+					<Pagination.Item>{10}</Pagination.Item>
+					<Pagination.Item>{11}</Pagination.Item>
+					<Pagination.Item active>{12}</Pagination.Item>
+					<Pagination.Item>{13}</Pagination.Item>
+					<Pagination.Item disabled>{14}</Pagination.Item>
+
+					<Pagination.Ellipsis />
+					<Pagination.Item>{20}</Pagination.Item>
+					<Pagination.Next />
+					<Pagination.Last />
+				</Pagination>
+			</Stack>
+		</Container>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-export * from "./components/Requirements";
+// export * from "./components/Requirements";
+export * from "./components/Example";
+// export * from "./components/SortableTable";
