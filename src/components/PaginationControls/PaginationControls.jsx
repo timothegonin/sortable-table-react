74725045b281f27268c8a3306bf0337409099535
TimotheGonin <gonin.timothe@gmail.com>
2023-08-22 14:37:11 +0200

    🏷️ 📝 documentation and proptypes

--- src/components/PaginationControls/PaginationControls.jsx
+++ src/components/PaginationControls/PaginationControls.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Stack from "react-bootstrap/Stack";
 import Badge from "react-bootstrap/Badge";
 import Pagination from "react-bootstrap/Pagination";
 
+/**
+ * Component for rendering pagination controls.
+ * @param {Object} props - The component props.
+ * @param {number} props.currentPage - The current page number.
+ * @param {function} props.setCurrentPage - Callback to set the current page.
+ * @param {number} props.itemsPerPage - Number of items to display per page.
+ * @param {Array} props.filteredData - The data to be paginated.
+ * @returns {JSX.Element} - JSX element representing the pagination controls.
+ */
 export const PaginationControls = ({
 	currentPage,
 	setCurrentPage,
@@ -18,11 +28,13 @@ export const PaginationControls = ({
 			gap={3}
 			className="my-3 d-flex flex-column flex-md-row justify-content-md-between"
 		>
+			{/* Display information about the currently shown items */}
 			<Badge bg="primary">
 				Showing {indexOfFirstItem + 1} to{" "}
 				{Math.min(indexOfLastItem, filteredData.length)} of{" "}
 				{filteredData.length} entries
 			</Badge>
+			{/* Pagination control */}
 			<Pagination size="sm" className="md-ms-auto my-auto">
 				<Pagination.First onClick={() => setCurrentPage(1)} />
 				<Pagination.Prev
@@ -55,4 +67,11 @@ export const PaginationControls = ({
 			</Pagination>
 		</Stack>
 	);
+};
+
+PaginationControls.propTypes = {
+	currentPage: PropTypes.number.isRequired,
+	setCurrentPage: PropTypes.func.isRequired,
+	itemsPerPage: PropTypes.number.isRequired,
+	filteredData: PropTypes.array.isRequired,
 };
